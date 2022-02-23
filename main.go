0391e7c35430abc1d58b9ba6383e2dbe6cf1eae0
MNoun <mnoun@student.bridgew.edu>
2022-02-22 21:27:39 -0500

    2nd Commit: txt file and main func

--- main.go
+++ main.go
@@ -6,14 +6,43 @@ Assignment: Project 2
 *************************/
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
 func main() {
 	//create channel and spawn the fortune function with the channel as a param
+	message := make(chan string)
+	fmt.Println("Your fortune is: ")
+	go fortune(message)
 
-	//forever loop for user
+	//for loop until user responds no
+	for true {
+		fmt.Println("Do you want another fortune? (yes/no)")
+		var response string
+		fmt.Scanln(&response) //gets input from user
+		response = strings.ToLower(response)
+		//if yes, send another fortune
+		if response == "yes" {
+			go fortune(message)
+		}
+		//if no, break out of loop
+		if response == "no" {
+			break
+		}
+	}
+	fmt.Println("Program has ended")
+	return
 }
 
-func fortune() {
-	//open fortunes.txt
-
+func fortune(message chan string) {
+	//open and read fortunes.txt
+	content, err := ioutil.ReadFile(Fortunes.txt)
+	if err != nil {
+		log.Fatal(err)
+	}
 	//split the contents of file
 }
